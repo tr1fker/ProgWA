5b79665c4fdc852b0b3fc33360f1d6aa7bede4f2
tr1fker <cfjhxhkxck@gmail.com>
2025-05-09 20:32:30 +0300

    TestDeletedLabWork8

--- LaboratoryWork8/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module tr1fker.laboratorywork8 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens tr1fker.laboratorywork8 to javafx.fxml;
-    exports tr1fker.laboratorywork8;
-}
